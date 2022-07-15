70acd88f655d17372b3313882ac4a4f272bbcdb1
Wilko Quak <w.quak@geonovum.nl>
2022-07-15 13:32:36 +0200

    Heel veel data toegevoegd.

--- data/TPOD.sql
+++ data/TPOD.sql
@@ -1,23 +1,20 @@
-delete from ReleasesBase where itemid = 'TPOD';
 insert into ReleasesBase(itemid,label,versienummer,downloaduri,releasedatum,status) VALUES
-    ('TPOD','A-tripelaccent','0.1.0-todo','https://www.geonovum.nl/geo-standaarden/omgevingswet/STOPTPOD','2019-07-10','definitief');
+    ('Geonovum/TPOD','A-tripelaccent','0.1.0-todo','https://www.geonovum.nl/geo-standaarden/omgevingswet/STOPTPOD','2019-07-10','definitief');
 
-DELETE FROM ReleaseDependencies
-where itemid like 'TPOD/%';
 
 INSERT INTO ReleaseDependencies(itemid,dependsonitem) values
-('TPOD/0.1.0-todo','IMOW/2.0.1'),
-('TPOD/0.1.0-todo','IMOW-XSD/2.0.0'),
-('TPOD/0.1.0-todo','TPOD-AMvB/2.0.1'),
-('TPOD/0.1.0-todo','TPOD-omgevingspan/2.1.0'),
-('TPOD/0.1.0-todo','TPOD-waterschapsverordening/2.0.1'),
-('TPOD/0.1.0-todo','TPOD-projectbesluit/2.0.1'),
-('TPOD/0.1.0-todo','TPOD-omgevingsvisie/2.0.1'),
-('TPOD/0.1.0-todo','Beheertool-waardelijsten/2.0.1'),
-('TPOD/0.1.0-todo','TPOD-reactieve-interventie/2.0.1'),
-('TPOD/0.1.0-todo','TPOD-voorbereidingsbesluit/2.0.1'),
-('TPOD/0.1.0-todo','TPOD-programma/2.0.1'),
-('TPOD/0.1.0-todo','Beheertool-waardelijsten/1.0.0'),
-('TPOD/0.1.0-todo','Beheertool-waardelijsten/1.1.0'),
-('TPOD/0.1.0-todo','TPOD-kennisgeving/1.1.0'),
-('TPOD/0.1.0-todo','TPOD-omgevingsverordening/2.0.1');
+('Geonovum/TPOD/0.1.0-todo','Geonovum/IMOW/2.0.1'),
+('Geonovum/TPOD/0.1.0-todo','Geonovum/IMOW-XSD/2.0.0'),
+('Geonovum/TPOD/0.1.0-todo','Geonovum/TPOD-AMvB/2.0.1'),
+('Geonovum/TPOD/0.1.0-todo','Geonovum/TPOD-omgevingspan/2.1.0'),
+('Geonovum/TPOD/0.1.0-todo','Geonovum/TPOD-waterschapsverordening/2.0.1'),
+('Geonovum/TPOD/0.1.0-todo','Geonovum/TPOD-projectbesluit/2.0.1'),
+('Geonovum/TPOD/0.1.0-todo','Geonovum/TPOD-omgevingsvisie/2.0.1'),
+('Geonovum/TPOD/0.1.0-todo','Geonovum/Beheertool-waardelijsten/2.0.1'),
+('Geonovum/TPOD/0.1.0-todo','Geonovum/TPOD-reactieve-interventie/2.0.1'),
+('Geonovum/TPOD/0.1.0-todo','Geonovum/TPOD-voorbereidingsbesluit/2.0.1'),
+('Geonovum/TPOD/0.1.0-todo','Geonovum/TPOD-programma/2.0.1'),
+('Geonovum/TPOD/0.1.0-todo','Geonovum/Beheertool-waardelijsten/1.0.0'),
+('Geonovum/TPOD/0.1.0-todo','Beheertool-waardelijsten/1.1.0'),
+('Geonovum/TPOD/0.1.0-todo','Geonovum/TPOD-kennisgeving/1.1.0'),
+('Geonovum/TPOD/0.1.0-todo','Geonovum/TPOD-omgevingsverordening/2.0.1');
--- printreleases.py
+++ printreleases.py
@@ -9,15 +9,26 @@ print()
 cur = con.cursor()
 for row in cur.execute('SELECT id,itemid,versienummer,label,downloaduri,releasedatum FROM Releases order by itemid,versienummer'):
     ident,itemid,versienummer,label,downloaduri,releasedatum = row
+    releaseid = itemid + '/' + versienummer
 
     print('## ' + itemid + " " + versienummer)
 
     print('|value|waarde|')
     print('|-----|------|')
-    print('| itemid  |' + itemid + '|');
-    print('| versienummer  |' + versienummer + '|');
+    print('| releaseid |' + releaseid)
     print('| label  |' + str(label) + '|');
     print('| downloaduri  |' + downloaduri + '|');
     print('| releasedatum  |' + str(releasedatum) + '|');
     print()
 
+    first = 1
+    cur = con.cursor()
+    for dep in cur.execute('SELECT itemid,dependsonitem FROM ReleaseDependencies where itemid = "' + releaseid + '"'):
+        itemid,dependson = dep;
+        if (first == 1):
+            first = 0
+            print('Is afhankelijk van:')
+            print()
+            print('|packagid|')
+            print('|--------|')
+        print('|' + dependson + '|')
