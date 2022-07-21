30013b443b4ec85d7a8a15eb3a0a0891e9607639
Wilko Quak <w.quak@geonovum.nl>
2022-07-21 16:37:53 +0200

    scripts iets verbeterd.

--- CreateWebsite.py
+++ CreateWebsite.py
@@ -1,18 +1,23 @@
 #!/usr/bin/python3
 #
-# Python program to convert sqlite DBMS into website that shows geonovum configuration items.
-# This is just a proof of concept.
-#   Author: Wilko Quak (w.quak@geonovum.nl)
+# Description: Python program to convert sqlite DBMS into website that shows geonovum configuration items.
+#    This is just a proof of concept.
+# Author: Wilko Quak (w.quak@geonovum.nl)
+# Date: 2022-07-22 Initial workable version
 #
 import sqlite3
 import sys
 import os
+
+#
+# open database with configuratie items
+#
 con = sqlite3.connect('ConfiguratieItems.sqlite')
-cur = con.cursor()
 
 
 #
-# Print an overview pf all BeheerItems owned by Geonovum
+# Create main index page in markdown for deployment site. Currently it contains a list of
+# all configuration items in the DBMS. Later this page gets convert to HTML.
 #
 indexfile = open("docs/index.md","wt")
 
@@ -20,6 +25,7 @@ indexfile.write("# Beheeritems van Geonovum\n\n")
 indexfile.write("Proof of concept van overzicht van beheeritems van Geonovum\n\n")
 indexfile.write ("|naam|type|\n")
 indexfile.write ("|----|----|\n")
+cur = con.cursor()
 for row in cur.execute('SELECT id, naam, omschrijving, organisatie, intern, releaselocatie, type FROM Beheeritems where intern == 0 and organisatie = "Geonovum" order by naam'):
     ide,naam,omschrijving,organisatie,internx,releaselocatie,typex = row
     indexfile.write("|[" + naam + "](" + ide + ")|" + typex + "|\n")
@@ -32,19 +38,26 @@ indexfile.close
 for row in cur.execute('SELECT id, naam, omschrijving, organisatie, intern, releaselocatie, type FROM Beheeritems where intern == 0 and organisatie = "Geonovum" order by naam'):
     id,naam,omschrijving,organisatie,internx,releaselocatie,typex = row
 
-    outfolder = "docs/" + id + ""
-    sys.stderr.write("outfile = " + outfolder + "/index.md\n");
+
+    #
+    # Query DBMS and find dependent package.
+    #
+    dependencies = [];
+    cur2 = con.cursor()
+    for dep in cur2.execute('SELECT itemid,dependsonitem FROM BeheeritemDependencies where itemid = "' + id + '"'):
+        itemid,dependson = dep;
+        dependencies.append(dependson);
+    #
+    # Create folder for package and open file
+    #
+    outfolder = "docs/" + id
     if not os.path.exists(outfolder):
         os.makedirs(outfolder)
     outfile = open(outfolder + "/index.md","wt")
 
-
-    dependencies = [];
-    cur = con.cursor()
-    for dep in cur.execute('SELECT itemid,dependsonitem FROM BeheeritemDependencies where itemid = "' + id + '"'):
-        itemid,dependson = dep;
-        dependencies.append(dependson);
-
+    #
+    # Print ci info.
+    #
     outfile.write('# ' + naam + '\n')
     outfile.write("\n")
     outfile.write(str(omschrijving) + '\n\n')
@@ -64,6 +77,10 @@ for row in cur.execute('SELECT id, naam, omschrijving, organisatie, intern, rele
     outfile.write('## Releases\n\n')
     outfile.write('|versienummer|releasedate|dependencies\n')
     outfile.write('|-------|-------|-----|\n')
+
+    #
+    # Find and print all releases of this ci.
+    #
     rcur = con.cursor()
     for release in rcur.execute('SELECT id,itemid,versienummer,label,downloaduri,releasedatum FROM Releases where itemid = "' + id + '" order by itemid,versienummer '):
         rident,ritemid,rversienummer,rlabel,rdownloaduri,rreleasedatum = release
